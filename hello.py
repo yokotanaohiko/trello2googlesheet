404647040e6065d661370635d99d50d2d4037dd3
iam4570 <iam4570@yahoo.co.jp>
2017-06-28 08:54:45 +0900

    fix

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return dict(request.args)
 
 if __name__ == '__main__':
     app.run()
