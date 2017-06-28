6498b0b0e29b5667e2115eb37fe7904160221164
iam4570 <iam4570@yahoo.co.jp>
2017-06-28 09:08:26 +0900

    fix

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print(json.dumps(dict(request.args), indent=2, ensure_ascii=False))
+    print(str(request.data))
     return str(dict(request.args))
 
 if __name__ == '__main__':
